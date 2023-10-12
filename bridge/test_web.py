39166e52e05b567d1bfbc798315302a58af61aee
Jonathan Melly <jonathan.melly@gmail.com>
2023-10-12 14:28:48 +0200

    feat(granted): look for markup in landed page

--- bridge/test_web.py
+++ bridge/test_web.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import logging
 
 from web import web_auth
 from dotenv import load_dotenv
@@ -8,12 +9,15 @@ from dotenv import load_dotenv
 class TestStringMethods(unittest.TestCase):
 
     def test_web_auth_denied(self):
+        logging.basicConfig(level=logging.DEBUG)
         self.assertFalse(web_auth("bob@eduvaud.ch", "marely"))
 
     def test_web_auth_granted(self):
+        logging.basicConfig(level=logging.DEBUG)
         load_dotenv()
         self.assertTrue(web_auth(os.getenv("ldap_user"), os.getenv("ldap_password")))
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     unittest.main()
--- bridge/web.py
+++ bridge/web.py
@@ -2,6 +2,7 @@ import logging
 import os
 import shelve
 
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -10,12 +11,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from dotenv import load_dotenv
 
+logger = logging.getLogger()
+
 
 def web_auth(username, password):
-    logging.log(logging.DEBUG, f"WEB auth request for {username}")
+    logger.debug(f"WEB auth request for {username}")
 
     cache = shelve.open("cache")
-    logging.log(logging.DEBUG, f"Cache loaded: {cache}")
+    logger.debug(f"Cache loaded: {cache}")
     cache.close()
 
     load_dotenv()
@@ -35,36 +38,36 @@ def web_auth(username, password):
         options.add_argument('--headless')
 
     driver = webdriver.Chrome(options=options)
-    logging.log(logging.DEBUG, f"WebDriver started {driver}")
-    #driver.implicitly_wait(int(os.getenv("wait", 5)))
+    logger.debug(f"WebDriver started {driver}")
+    # driver.implicitly_wait(int(os.getenv("wait", 5)))
 
     url = os.getenv("portal_url")
-    logging.log(logging.DEBUG, f"Loading URL {url}")
+    logger.debug(f"Loading URL {url}")
     driver.get(url)
-    logging.log(logging.DEBUG, f"Loading URL {url} DONE")
+    logger.debug(f"Loading URL {url} DONE")
     # html = driver.page_source
 
-    ttl = int(os.getenv("ttl", 30))
+    ttl = int(os.getenv("ttl", 10))
 
     # Global ms User part
     xusername = os.getenv("xusername")
-    logging.log(logging.DEBUG, f"waiting for username field {xusername}")
+    logger.debug(f"waiting for username field {xusername}")
     field_username = WebDriverWait(driver, ttl).until(
         EC.presence_of_element_located((By.XPATH, xusername)))
-    logging.log(logging.DEBUG, "->DONE")
+    logger.debug("->DONE")
 
-    logging.log(logging.DEBUG, f"filling {field_username}")
+    logger.debug(f"filling {field_username}")
     field_username.send_keys(username)
-    logging.log(logging.DEBUG, "->DONE")
+    logger.debug("->DONE")
 
     xsubmit1 = os.getenv("xsubmit1")
-    logging.log(logging.DEBUG, f"waiting for submit {xsubmit1}")
+    logger.debug(f"waiting for submit {xsubmit1}")
     button_submit = driver.find_element(By.XPATH, xsubmit1)
-    logging.log(logging.DEBUG, "->DONE")
+    logger.debug("->DONE")
 
-    logging.log(logging.DEBUG, f"clicking on {button_submit}")
+    logger.debug(f"clicking on {button_submit}")
     button_submit.click()
-    logging.log(logging.DEBUG, "->DONE")
+    logger.debug("->DONE")
 
     # Custom portal
 
@@ -72,35 +75,34 @@ def web_auth(username, password):
     login_url = driver.current_url
 
     xpassword = os.getenv("xpassword")
-    logging.log(logging.DEBUG, f"waiting for password field {xpassword}")
+    logger.debug(f"waiting for password field {xpassword}")
     field_password = WebDriverWait(driver, ttl).until(
         EC.presence_of_element_located((By.XPATH, xpassword)))
-    logging.log(logging.DEBUG, f"filling {xpassword}")
+    logger.debug(f"filling {xpassword}")
     field_password.send_keys(password)
-    logging.log(logging.DEBUG, "->DONE")
+    logger.debug("->DONE")
 
-    xsubmit2=os.getenv("xsubmit2")
-    logging.log(logging.DEBUG, f"waiting for {xsubmit2}")
+    xsubmit2 = os.getenv("xsubmit2")
+    logger.debug(f"waiting for {xsubmit2}")
     button_submit = WebDriverWait(driver, ttl).until(
         EC.presence_of_element_located((By.XPATH, xsubmit2)))
-    logging.log(logging.DEBUG, "->DONE")
+    logger.debug("->DONE")
 
-    logging.log(logging.DEBUG, f"clicking on {button_submit}")
+    logger.debug(f"clicking on {button_submit}")
     button_submit.click()
-    logging.log(logging.DEBUG, "->DONE")
+    logger.debug("->DONE")
 
-    xlogout = os.getenv("xlogout")
-    logging.log(logging.DEBUG, f"waiting for {xlogout}")
-    logout = WebDriverWait(driver, ttl).until(
-        EC.presence_of_element_located((By.XPATH,xlogout )))
-    logging.log(logging.DEBUG, "->DONE")
+    try:
+        xlanded = os.getenv("xlanded")
+        logger.debug(f"waiting for {xlanded}")
+        WebDriverWait(driver, ttl).until(EC.presence_of_element_located((By.XPATH, xlanded)))
+        logger.debug("->DONE")
+    except TimeoutException:
+        logger.debug("->TIMEOUT")
+        return False
 
     landed_url = driver.current_url.lower()
     # if login success => go to microsoft portal o365, otherwise stays on eduvaud sts
-    logging.log(logging.DEBUG, f"Landed url for   {username}: {landed_url}")
-
-    logging.log(logging.DEBUG, f"clicking on {logout}")
-    logout.click()
-    logging.log(logging.DEBUG, "->DONE")
+    logger.debug(f"Landed url: {landed_url}")
 
     return landed_url != login_url and os.getenv("landed_url_pattern") in landed_url.lower()
